cb884c419378fa35eac3fb0452c6d911935a3f76
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-10-22 15:44:48 +0100

    Add Index action to Customers controller

---
+++ VidlyCoreAuth/Controllers/CustomersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VidlyCoreAuth.Models;
+using VidlyCoreAuth.ViewModels;
+
+namespace VidlyCoreAuth.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public IActionResult Index()
+        {
+            var customers = new List<Customer>() {
+                new Customer { Name = "John Smith", Id = 1 },
+                new Customer { Name = "Mary Williams" , Id = 2}
+            };
+
+            var viewModel = new CustomerViewModel() {
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+    }
+}
