9fb743802eb1c11cc54a1c8f28227044588a2d19
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-10-22 13:49:49 +0100

    Update Random action and view to use viewmodel

--- VidlyCoreAuth/Controllers/MoviesController.cs
+++ VidlyCoreAuth/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using VidlyCoreAuth.Models;
+using VidlyCoreAuth.ViewModels;
 
 namespace VidlyCoreAuth.Controllers
 {
@@ -13,7 +14,17 @@ namespace VidlyCoreAuth.Controllers
         public IActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+
+            var customers = new List<Customer>() {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel() {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
         }
     }
 }
