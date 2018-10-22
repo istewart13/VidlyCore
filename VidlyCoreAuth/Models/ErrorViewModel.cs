e565ae6355bcc2dc7e9bf90f03c5f059a00ef9d9
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-10-22 10:42:37 +0100

    Add project files.

---
+++ VidlyCoreAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VidlyCoreAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
