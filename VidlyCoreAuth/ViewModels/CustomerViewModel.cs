d83a4c6aa7110c3db838ab125edbfc3fb7a36262
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-10-22 15:44:17 +0100

    Add CustomerViewModel

---
+++ VidlyCoreAuth/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VidlyCoreAuth.Models;
+
+namespace VidlyCoreAuth.ViewModels
+{
+    public class CustomerViewModel
+    {
+        public List<Customer> Customers { get; set; }
+    }
+}
