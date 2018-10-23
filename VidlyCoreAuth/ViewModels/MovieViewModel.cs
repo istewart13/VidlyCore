c8db380e453da604054d7318148048637b453a02
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-10-23 08:49:24 +0100

    Add MovieViewModel, and Details/Index view

---
+++ VidlyCoreAuth/ViewModels/MovieViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VidlyCoreAuth.Models;
+
+namespace VidlyCoreAuth.ViewModels
+{
+    public class MovieViewModel
+    {
+        public List<Movie> Movies { get; set; }
+    }
+}
