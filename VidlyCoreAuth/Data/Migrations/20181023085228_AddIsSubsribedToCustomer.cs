317c2b4e7752464db213f7b2a90ee7485e53a9d6
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-10-23 09:53:38 +0100

    Add DB migration

---
+++ VidlyCoreAuth/Data/Migrations/20181023085228_AddIsSubsribedToCustomer.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace VidlyCoreAuth.Data.Migrations
+{
+    public partial class AddIsSubsribedToCustomer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSubscribedToNewsletter",
+                table: "Customers",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSubscribedToNewsletter",
+                table: "Customers");
+        }
+    }
+}
