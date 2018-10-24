6e7cbfdeae84264445e75f6725e9a75efb4d2fa1
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-10-24 11:15:59 +0100

    Update controller to get data from DB

--- VidlyCoreAuth/Controllers/CustomersController.cs
+++ VidlyCoreAuth/Controllers/CustomersController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VidlyCoreAuth.Data;
 using VidlyCoreAuth.Models;
 using VidlyCoreAuth.ViewModels;
 
@@ -10,9 +12,21 @@ namespace VidlyCoreAuth.Controllers
 {
     public class CustomersController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public IActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.ToList();
 
             var viewModel = new CustomerViewModel() {
                 Customers = customers
@@ -24,21 +38,12 @@ namespace VidlyCoreAuth.Controllers
         [Route("customers/details/{id}")]
         public IActionResult Details(int id)
         {
-            var customer = GetCustomers().FirstOrDefault(cust => cust.Id == id);
+            var customer = _context.Customers.SingleOrDefault(cust => cust.Id == id);
             if (customer == null)
             {
                 return NotFound();
             }
             return View(customer);
-        }
-
-        private List<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "John Smith" },
-                new Customer { Id = 2, Name = "Mary Williams" }
-            };
         }
     }
 }
