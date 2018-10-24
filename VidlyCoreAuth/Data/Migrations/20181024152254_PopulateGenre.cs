b662569014fa7f10a01c8c4da06c2f3d256b04cb
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-10-24 16:28:39 +0100

    Populate Genre data

---
+++ VidlyCoreAuth/Data/Migrations/20181024152254_PopulateGenre.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace VidlyCoreAuth.Data.Migrations
+{
+    public partial class PopulateGenre : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Genre (Id, Name) VALUES (1, 'Action')");
+            migrationBuilder.Sql("INSERT INTO Genre (Id, Name) VALUES (2, 'Thriller')");
+            migrationBuilder.Sql("INSERT INTO Genre (Id, Name) VALUES (3, 'Family')");
+            migrationBuilder.Sql("INSERT INTO Genre (Id, Name) VALUES (4, 'Romance')");
+            migrationBuilder.Sql("INSERT INTO Genre (Id, Name) VALUES (5, 'Comedy')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
