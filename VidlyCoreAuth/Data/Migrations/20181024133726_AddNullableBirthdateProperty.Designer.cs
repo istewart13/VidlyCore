378a3ff3b0569c8ffac72093f097c293d66a4b91
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-10-24 14:39:35 +0100

    Add birthdate to Customer

--- VidlyCoreAuth/Data/Migrations/20181024133726_AddNullableBirthdateProperty.Designer.cs
+++ VidlyCoreAuth/Data/Migrations/20181024133726_AddNullableBirthdateProperty.Designer.cs
@@ -11,9 +11,10 @@ using VidlyCoreAuth.Data;
 namespace VidlyCoreAuth.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181024133726_AddNullableBirthdateProperty")]
+    partial class AddNullableBirthdateProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
