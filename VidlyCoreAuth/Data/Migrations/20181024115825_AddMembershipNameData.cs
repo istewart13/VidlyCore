87776d64d769fc04be3553776330693508c7eb70
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-10-24 13:26:59 +0100

    Add name data to MembershipType

---
+++ VidlyCoreAuth/Data/Migrations/20181024115825_AddMembershipNameData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace VidlyCoreAuth.Data.Migrations
+{
+    public partial class AddMembershipNameData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE MembershipType SET Name = 'Pay As You Go' WHERE Id = 1");
+            migrationBuilder.Sql("UPDATE MembershipType SET Name = 'Monthly' WHERE Id = 2");
+            migrationBuilder.Sql("UPDATE MembershipType SET Name = 'Quarterly' WHERE Id = 3");
+            migrationBuilder.Sql("UPDATE MembershipType SET Name = 'Annual' WHERE Id = 4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
