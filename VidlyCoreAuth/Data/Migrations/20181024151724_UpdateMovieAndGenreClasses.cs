be735ec254c4e9899c750d15f417c098e77fd2ab
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-10-24 16:20:59 +0100

    Add additional fields to movie

---
+++ VidlyCoreAuth/Data/Migrations/20181024151724_UpdateMovieAndGenreClasses.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace VidlyCoreAuth.Data.Migrations
+{
+    public partial class UpdateMovieAndGenreClasses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Movies",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateAdded",
+                table: "Movies",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<byte>(
+                name: "GenreId",
+                table: "Movies",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "NumberInStock",
+                table: "Movies",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "Movies",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateTable(
+                name: "Genre",
+                columns: table => new
+                {
+                    Id = table.Column<byte>(nullable: false),
+                    Name = table.Column<string>(maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genre", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_GenreId",
+                table: "Movies",
+                column: "GenreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_Genre_GenreId",
+                table: "Movies",
+                column: "GenreId",
+                principalTable: "Genre",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_Genre_GenreId",
+                table: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "Genre");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Movies_GenreId",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "DateAdded",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "GenreId",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "NumberInStock",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "ReleaseDate",
+                table: "Movies");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Movies",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255);
+        }
+    }
+}
---
+++ VidlyCoreAuth/Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VidlyCoreAuth.Models
+{
+    public class Genre
+    {
+        public byte Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+    }
+}
