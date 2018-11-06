1bd0542037348fb985da15c9a0aa9e3a7b058942
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-11-06 12:37:31 +0000

    Add Genres to db context and add annotations to fields

--- VidlyCoreAuth/Data/ApplicationDbContext.cs
+++ VidlyCoreAuth/Data/ApplicationDbContext.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,6 +13,7 @@ namespace VidlyCoreAuth.Data
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Movie> Movies { get; set; }
         public DbSet<MembershipType> MembershipTypes { get; set; }
+        public DbSet<Genre> Genres { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
