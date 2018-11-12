b7df0f02570f927a2ecd59db1f49ed21c295c5df
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-11-12 10:17:39 +0000

    Add customer validation to check if members are >= 18 years old

--- VidlyCoreAuth/Models/MembershipType.cs
+++ VidlyCoreAuth/Models/MembershipType.cs
@@ -12,7 +12,12 @@ namespace VidlyCoreAuth.Models
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
+
         [StringLength(30)]
+        [Required]
         public string Name { get; set; }
+
+        public static readonly byte Unknown = 0;
+        public static readonly byte PayAsYouGo = 1;
     }
 }
